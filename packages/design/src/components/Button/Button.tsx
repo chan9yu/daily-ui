e0a4b3cf57776f978a989bf22f9c84e9cddb564e
changyuyeo <wry5533@gmail.com>
2022-06-09 14:12:50 +0900

    chore: prettier script tnwjd

--- packages/design/src/components/Button/Button.tsx
+++ packages/design/src/components/Button/Button.tsx
@@ -1,15 +1,15 @@
-import React, { ButtonHTMLAttributes, FC, ReactNode } from "react";
+import React, { ButtonHTMLAttributes, FC, ReactNode } from 'react';
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-	children: ReactNode;
+  children: ReactNode;
 }
 
 const Button: FC<ButtonProps> = ({ children, ...props }) => {
-	return (
-		<button className="rsup-button__container" {...props}>
-			{children}
-		</button>
-	);
+  return (
+    <button className="rsup-button__container" {...props}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import styled from "styled-components";
-import emotion from "@emotion/styled";
+import React from 'react';
+import styled from 'styled-components';
+import emotion from '@emotion/styled';
 
-import { Button } from "@rsup/design";
-import "@rsup/styles/build/button.css";
+import { Button } from '@rsup/design';
+import '@rsup/styles/build/button.css';
 
 const EmotionStyle = emotion(Button)`
 	font-size: 50px;
@@ -12,20 +12,20 @@ const EmotionStyle = emotion(Button)`
 `;
 
 const ButtonStyled = styled(Button)`
-	font-size: 50px;
-	/* background-color: crimson; */
-	background-color: royalblue;
+  font-size: 50px;
+  /* background-color: crimson; */
+  background-color: royalblue;
 `;
 
 const App = () => {
-	return (
-		<>
-			<EmotionStyle>emotion</EmotionStyle>
-			<br />
-			<br />
-			<ButtonStyled>styled</ButtonStyled>
-		</>
-	);
+  return (
+    <>
+      <EmotionStyle>emotion</EmotionStyle>
+      <br />
+      <br />
+      <ButtonStyled>styled</ButtonStyled>
+    </>
+  );
 };
 
 export default App;
