711aa3a83c6e9d4d30b358e72d243270c6ab1f2a
changyuyeo <wry5533@gmail.com>
2022-06-09 13:48:06 +0900

    chore: 설정 변경

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,17 +1,31 @@
 import React from "react";
 import styled from "styled-components";
+import emotion from "@emotion/styled";
 
 import { Button } from "@rsup/design";
 import "@rsup/styles/build/button.css";
 
-const ButtonStyle = styled(Button)`
+const EmotionStyle = emotion(Button)`
 	font-size: 50px;
 	background-color: crimson;
 	/* background-color: royalblue; */
 `;
 
+const ButtonStyled = styled(Button)`
+	font-size: 50px;
+	/* background-color: crimson; */
+	background-color: royalblue;
+`;
+
 const App = () => {
-	return <ButtonStyle>test</ButtonStyle>;
+	return (
+		<>
+			<EmotionStyle>emotion</EmotionStyle>
+			<br />
+			<br />
+			<ButtonStyled>styled</ButtonStyled>
+		</>
+	);
 };
 
 export default App;
