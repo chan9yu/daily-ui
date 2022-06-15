d8ebf83829b5bdde058116b0fa47e2f40f2923a5
changyuyeo <wry5533@gmail.com>
2022-06-15 22:34:08 +0900

    feat: scss 변수 수정

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import { Button } from '@rsup/design';
+import styled from 'styled-components';
+import emotion from '@emotion/styled';
+
+const MyButton = styled(Button)`
+  background-color: red;
+`;
+
+const MyButton2 = emotion(Button)`
+  background-color: #333;
+`;
 
 const App = () => {
-  return <>Normal</>;
+  return (
+    <>
+      <Button onClick={() => console.log('test')} type="button">
+        Normal
+      </Button>
+      <br />
+      <MyButton2>Normal</MyButton2>
+      <br />
+      <MyButton>Normal</MyButton>
+    </>
+  );
 };
 
 export default App;
