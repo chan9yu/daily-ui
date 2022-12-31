75cef371eedb4592fd9be0b3f169543cb8b0d142
여찬규 <wry5533@gmail.com>
2022-12-31 21:30:26 +0900

    feat: project clear

--- packages/react-ui/src/molecules/Select/props.data.ts
+++
@@ -1,9 +0,0 @@
-const options = [
-  { value: '1', label: '하나' },
-  { value: '2', label: '둘' },
-  { value: '21', label: '둘' },
-  { value: '22', label: '둘' },
-  { value: '23', label: '둘' },
-];
-
-export default options;
--- scripts/publish.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-echo -e "\033[33m" --- npm publish ---"\033[m"
-source .env
-
-yarn lerna publish --registry $BASE_REGISTRY_URIscripts/clean-modules.sh
