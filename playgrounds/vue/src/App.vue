1d2232e8ff8d0e6f632758ec265548f7940e9d90
여찬규 <cgyeo@rsupport.com>
2023-03-24 20:19:19 +0900

    feat: added playground with vuejs

---
+++ playgrounds/vue/src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+	<div>VUE TEST</div>
+</template>
+
+<style scoped>
+.container {
+	background-color: #eee;
+	width: 100vw;
+	height: 100vh;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+</style>
